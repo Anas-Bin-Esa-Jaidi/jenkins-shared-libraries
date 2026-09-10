97e2ee345f24959fbb1373704271bcfa7b12dc88
Anas Bin Esa <anasanas51808@gmail.com>
2026-09-10 11:03:19 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,3 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  echo "Code is being Build"
-  sh "whoami"
-  sh "docker build -t notes-app:latest ."
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
